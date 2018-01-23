938b698ef3babd40709d1a9ac57733c85ff2fffe
igorventurelli <igor.venturelli@me.com>
2018-01-23 20:25:18 -0200

    Removing the Spring Security from Service layer

--- controller/src/main/java/br/com/krustytm/controller/endpoint/HomeController.java
+++ controller/src/main/java/br/com/krustytm/controller/endpoint/HomeController.java
@@ -1,10 +1,17 @@
 package br.com.krustytm.controller.endpoint;
 
+import br.com.krustytm.dto.dtos.UserDTO;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
-
+    @RequestMapping("/")
+    public UserDTO index() {
+        UserDTO user = new UserDTO();
+        user.setName("Igor");
+        user.setUsername("igorventurelli");
+        return user;
+    }
 }
