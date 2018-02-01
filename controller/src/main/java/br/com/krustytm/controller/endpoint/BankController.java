5f5878e319c840d877a843bef1a24be18528d2c7
igorventurelli <igor.venturelli@me.com>
2018-01-31 23:01:59 -0200

    Adding CrossOrigin

--- controller/src/main/java/br/com/krustytm/controller/endpoint/BankController.java
+++ controller/src/main/java/br/com/krustytm/controller/endpoint/BankController.java
@@ -3,6 +3,7 @@ package br.com.krustytm.controller.endpoint;
 import br.com.krustytm.dto.dtos.BankDTO;
 import br.com.krustytm.service.services.BankService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/bank")
+@CrossOrigin(origins = "*")
 public class BankController {
 
     @Autowired
--- controller/src/main/java/br/com/krustytm/controller/endpoint/UserController.java
+++ controller/src/main/java/br/com/krustytm/controller/endpoint/UserController.java
@@ -3,12 +3,14 @@ package br.com.krustytm.controller.endpoint;
 import br.com.krustytm.dto.dtos.UserDTO;
 import br.com.krustytm.service.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
+@CrossOrigin(origins = "*")
 public class UserController {
 
     @Autowired
